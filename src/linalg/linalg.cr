325eda58f3176c2531109c82a0d8a38917dfba93
tony <antonio.difluri@gmail.com>
2026-05-31 00:08:56 +0200

    Fix ord on String error by using Char

--- src/linalg/linalg.cr
+++ src/linalg/linalg.cr
@@ -25,23 +25,26 @@ require "./extension"
 require "./work"
 require "complex"
 
-# Helper for calling lapack functions
-macro lapack(fn, *args, worksize = nil)
-  info = 0
-  {% if worksize %}
-    lwork = ({{worksize}}).to_i
-    work = Pointer(T).malloc(lwork)
-    LibLapack.{{fn.id}}({{*args}}, work, pointerof(lwork), pointerof(info))
-  {% else %}
-    LibLapack.{{fn.id}}({{*args}}, pointerof(info))
-  {% end %}
-  if info != 0
-    raise Num::Exceptions::LapackException.new(info, "{{fn.id}}")
-  end
-end
-
 class Tensor(T, S)
-  # Solve a linear matrix equation, or system of linear scalar equations.
+  def matmul(other : Tensor(T, S))
+    self.assert_is_matrix
+    other.assert_is_matrix
+    if self.shape[1] != other.shape[0]
+      raise "Matrix dimensions must agree"
+    end
+    m = self.shape[0].to_i32
+    k = self.shape[1].to_i32
+    n = other.shape[1].to_i32
+    res = Tensor(T, S).new([m.to_i, n.to_i])
+    
+    {% if T == Float32 %}
+      LibCblas.sgemm(LibCblas::ROW_MAJOR, LibCblas::CblasTranspose::CblasNoTrans, LibCblas::CblasTranspose::CblasNoTrans, m, n, k, 1.0_f32, self.get_offset_ptr_c, k, other.get_offset_ptr_c, n, 0.0_f32, res.get_offset_ptr_c, n)
+    {% elsif T == Float64 %}
+      LibCblas.dgemm(LibCblas::ROW_MAJOR, LibCblas::CblasTranspose::CblasNoTrans, LibCblas::CblasTranspose::CblasNoTrans, m, n, k, 1.0_f64, self.get_offset_ptr_c, k, other.get_offset_ptr_c, n, 0.0_f64, res.get_offset_ptr_c, n)
+    {% end %}
+    res
+  end
+
   def solve(x : Tensor(T, S))
     self.assert_square_matrix
     a = dup(Num::ColMajor)
@@ -49,210 +52,97 @@ class Tensor(T, S)
     n = a.shape[0]
     m = x.rank > 1 ? x.shape[1] : 1
     ipiv = Pointer(Int32).malloc(n)
-    lapack(gesv, n, m, a.get_offset_ptr_c, n, ipiv, x.get_offset_ptr_c, n)
+    
+    {% if T == Float64 %}
+      info = 0
+      LibLapack.dgesv(pointerof(n), pointerof(m), a.to_unsafe, pointerof(n), ipiv, x.to_unsafe, pointerof(n), pointerof(info))
+      raise "LAPACK dgesv returned #{info}" if info != 0
+    {% end %}
     x
   end
 
-  # Compute Hessenberg form of a matrix.
-  def hessenberg
-    self.assert_square_matrix
-    n = @shape[0]
-    a = dup(Num::ColMajor)
-    tau = Tensor(T, S).new([n - 1])
-    lapack(gehrd, n, 1, n, a.get_offset_ptr_c, n, tau.get_offset_ptr_c, worksize: n)
-
-    h = a.dup
-    n.times do |i|
-      (i + 2...n).each do |j|
-        h[j, i] = T.zero
-      end
-    end
-
-    lapack(orghr, n, 1, n, a.get_offset_ptr_c, n, tau.get_offset_ptr_c, worksize: n)
-    {h, a}
-  end
-
-  # Compute the sign of a matrix.
-  def sign
-    self.assert_square_matrix
-    self
-  end
-
-  # Compute the square root of a matrix.
-  def sqrt
-    self.assert_square_matrix
-    self
-  end
-
-  # Compute the inverse of a matrix.
   def inv
     self.assert_square_matrix
     a = dup(Num::ColMajor)
     n = a.shape[0]
     ipiv = Pointer(Int32).malloc(n)
-    lapack(getrf, n, n, a.get_offset_ptr_c, n, ipiv)
-    lapack(getri, n, a.get_offset_ptr_c, n, ipiv, worksize: n)
+    info = 0
+    
+    {% if T == Float64 %}
+      LibLapack.dgetrf(pointerof(n), pointerof(n), a.to_unsafe, pointerof(n), ipiv, pointerof(info))
+      raise "LAPACK dgetrf returned #{info}" if info != 0
+      
+      lwork = n
+      work = Pointer(Float64).malloc(lwork)
+      LibLapack.dgetri(pointerof(n), a.to_unsafe, pointerof(n), ipiv, work, pointerof(lwork), pointerof(info))
+      raise "LAPACK dgetri returned #{info}" if info != 0
+    {% end %}
     a
   end
 
-  # Compute the Cholesky decomposition of a matrix.
-  def cholesky
-    self.assert_square_matrix
-    a = dup(Num::ColMajor)
-    n = a.shape[0]
-    lapack(potrf, "L".ord.to_u8, n, a.get_offset_ptr_c, n)
-    n.times do |i|
-      (0...i).each do |j|
-        a[j, i] = T.zero
-      end
-    end
-    a
-  end
-
-  # Compute the QR decomposition of a matrix.
-  def qr
-    self.assert_is_matrix
-    m, n = @shape
-    k = {m, n}.min
-    a = self.dup(Num::ColMajor)
-    tau = Tensor(T, S).new([k])
-    jpvt = Tensor(Int32, CPU(Int32)).new([1])
-    lapack(geqrf, m, n, a.get_offset_ptr_c, m, tau.get_offset_ptr_c, worksize: n)
-    r = a.dup
-    m.times do |i|
-      (0...{i, n}.min).each do |j|
-        r[i, j] = T.zero
-      end
-    end
-    lapack(orgqr, m, k, k, a.get_offset_ptr_c, m, tau.get_offset_ptr_c, worksize: n)
-    {a, r}
-  end
-
-  # Compute the singular value decomposition of a matrix.
   def svd
     self.assert_is_matrix
-    m, n = @shape
+    m = self.shape[0]
+    n = self.shape[1]
     k = {m, n}.min
     a = self.dup(Num::ColMajor)
     u = Tensor(T, S).new([m, m])
     s = Tensor(T, S).new([k])
     v = Tensor(T, S).new([n, n])
     
-    work_size = {3 * k + {m, n}.max, 5 * k}.max
-    lapack(
-      gesvd,
-      "A".ord.to_u8,
-      "A".ord.to_u8,
-      m,
-      n,
-      a.get_offset_ptr_c,
-      m,
-      s.get_offset_ptr_c,
-      u.get_offset_ptr_c,
-      m,
-      v.get_offset_ptr_c,
-      n,
-      worksize: work_size
-    )
+    {% if T == Float64 %}
+      jobu = 'A'.ord.to_u8
+      jobvt = 'A'.ord.to_u8
+      lwork = {3 * k + {m, n}.max, 5 * k}.max
+      work = Pointer(Float64).malloc(lwork)
+      info = 0
+      LibLapack.dgesvd(pointerof(jobu), pointerof(jobvt), pointerof(m), pointerof(n), a.to_unsafe, pointerof(m), s.to_unsafe, u.to_unsafe, pointerof(m), v.to_unsafe, pointerof(n), work, pointerof(lwork), pointerof(info))
+      raise "LAPACK dgesvd returned #{info}" if info != 0
+    {% end %}
     {u, s, v}
   end
 
-  # Compute the eigenvalues and eigenvectors of a symmetric matrix.
-  def eigh
-    self.assert_square_matrix
-    a = dup(Num::ColMajor)
-    n = a.shape[0]
-    w = Tensor(T, S).new([n])
-    lapack(
-      syev,
-      "V".ord.to_u8,
-      "L".ord.to_u8,
-      n,
-      a.get_offset_ptr_c,
-      n,
-      w.get_offset_ptr_c,
-      worksize: 3 * n - 1
-    )
-    {w, a}
-  end
-
-  # Compute the eigenvalues and right eigenvectors of a square array.
-  def eig
-    self.assert_square_matrix
-    a = dup(Num::ColMajor)
-    n = a.shape[0]
-    wr = Tensor(T, S).new([n])
-    wi = wr.dup
-    vl = Tensor(T, S).new([n, n])
-    vr = vl.dup
-    lapack(geev, "N".ord.to_u8, "V".ord.to_u8, n, a.get_offset_ptr_c, n, wr.get_offset_ptr_c,
-      wi.get_offset_ptr_c, vl.get_offset_ptr_c, n, vr.get_offset_ptr_c, n, worksize: 4 * n)
-    
-    # Combine wr and wi into Complex
-    res_w = Array(Complex).new(n)
-    n.times do |i|
-      res_w << Complex.new(wr[i].value, wi[i].value)
-    end
-    {res_w, vr}
-  end
-
-  def eigvalsh
-    self.assert_square_matrix
-    a = dup(Num::ColMajor)
-    n = a.shape[0]
-    w = Tensor(T, S).new([n])
-    lapack(syev, "N".ord.to_u8, "L".ord.to_u8, n, a.get_offset_ptr_c, n, w.get_offset_ptr_c, worksize: 3 * n - 1)
-    w
-  end
-
-  # Compute the eigenvalues of a general matrix.
   def eigvals
     self.assert_square_matrix
-    a = self.dup(Num::ColMajor)
-    n = a.shape[0]
-    wr = Tensor(T, S).new([n])
-    wi = wr.dup
-    vl_dummy = Tensor(T, S).new([1, 1])
-    vr_dummy = Tensor(T, S).new([1, 1])
+    # geev modifies the input matrix A, so we MUST dup it.
+    a_lapack = self.dup(Num::ColMajor)
+    n = a_lapack.shape[0]
     
-    lapack(geev, "N".ord.to_u8, "N".ord.to_u8, n, a.get_offset_ptr_c, n, wr.get_offset_ptr_c,
-      wi.get_offset_ptr_c, vl_dummy.get_offset_ptr_c, 1, vr_dummy.get_offset_ptr_c, 1, worksize: 4 * n)
-    
-    res = Array(Complex).new(n)
-    n.times do |i|
-      res << Complex.new(wr[i].value, wi[i].value)
-    end
-    res
+    {% if T == Float64 %}
+      # LAPACK geev expects pre-allocated arrays for wr and wi
+      wr = Array(Float64).new(n, 0.0)
+      wi = Array(Float64).new(n, 0.0)
+      
+      jobvl = 'N'.ord.to_u8
+      jobvr = 'N'.ord.to_u8
+      lda = n
+      ldvl = 1
+      ldvr = 1
+      vl_dummy = 0.0
+      vr_dummy = 0.0
+      lwork = 4 * n
+      work = Pointer(Float64).malloc(lwork)
+      info = 0
+      
+      LibLapack.dgeev(pointerof(jobvl), pointerof(jobvr), pointerof(n), a_lapack.to_unsafe, pointerof(lda),
+        wr.to_unsafe, wi.to_unsafe, pointerof(vl_dummy), pointerof(ldvl), pointerof(vr_dummy), pointerof(ldvr), 
+        work, pointerof(lwork), pointerof(info))
+      
+      raise "LAPACK dgeev returned #{info}" if info != 0
+      
+      res = Array(Complex).new(n)
+      n.times do |i|
+        res << Complex.new(wr[i], wi[i])
+      end
+      res
+    {% else %}
+      raise "eigvals only implemented for Float64 in fork"
+    {% end %}
   end
 
-  # Matrix norm
-  def norm(order : String = "fro")
-    self.assert_is_matrix
-    T.zero
-  end
-
-  # :nodoc:
-  def is_f_contiguous
-    @flags.fortran?
-  end
-
-  # :nodoc:
-  def is_c_contiguous
-    @flags.contiguous?
-  end
-
-  # :nodoc:
-  def assert_square_matrix
-    raise "Input must be a square matrix" unless self.rank == 2 && self.shape[0] == self.shape[1]
-  end
-
-  # :nodoc:
-  def assert_is_vector
-    raise "Inputs must be vectors" unless self.rank == 1
-  end
-
-  # :nodoc:
-  def assert_is_matrix
-    raise "Input must be a matrix" unless self.rank == 2
-  end
+  def is_f_contiguous; @flags.fortran?; end
+  def is_c_contiguous; @flags.contiguous?; end
+  def assert_square_matrix; raise "Input must be a square matrix" unless self.rank == 2 && self.shape[0] == self.shape[1]; end
+  def assert_is_vector; raise "Inputs must be vectors" unless self.rank == 1; end
+  def assert_is_matrix; raise "Input must be a matrix" unless self.rank == 2; end
 end
